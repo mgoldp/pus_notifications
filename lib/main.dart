1073fbeb4e493b59a02e93464eeed96b2651ab8a
mgoldp <mgoldp2013@gmail.com>
2022-11-14 18:58:50 -0300

    fist commit base project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:push_notifications/screens/home_screen.dart';
+import 'package:push_notifications/screens/message_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'message': (_) => const MessageScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/message_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MessageScreen extends StatelessWidget {
+  const MessageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Message Screen'),
+      ),
+      body: const Center(
+        child: Text(
+          'Message Screen',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
