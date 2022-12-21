5e0a6ebbf07dfaa43e8bf01841d000e25bfcca66
mgoldp <mgoldp2013@gmail.com>
2022-12-21 12:11:41 -0300

    generate token

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:push_notifications/screens/home_screen.dart';
 import 'package:push_notifications/screens/message_screen.dart';
+import 'package:push_notifications/services/push_notifications_service.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PushNotificationService.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,1 +1,18 @@
 // SHA 1 1E:06:46:04:16:7D:6D:41:D1:5C:DA:CB:D6:2E:BC:84:D0:AB:E4:1A
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+
+  static Future initializeApp() async {
+    // Push notifications
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+    print(token);
+
+    // local notifications
+  }
+}
